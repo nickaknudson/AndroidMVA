ca265b84286ad9adfecb6f698de9780f06e29716
Nick Knudson <nickaknudson@gmail.com>
2014-01-02 15:29:36 -0800

    Adapter is a simple interface now used for convention

---
+++ src/com/nickaknudson/mva/adapters/Adapter.java
@@ -0,0 +1,13 @@
+package com.nickaknudson.mva.adapters;
+
+
+/**
+ * An adapter defines a logical set of business logic. The primary use case
+ * being an intermediary between a {@link com.nickaknudson.mva.Model} and a
+ * View. While this is the primary usage, an Adapter does not need to present a
+ * UI. A good way to think of an Adapter is as a lightweight Fragment.
+ * 
+ * @author nick
+ *
+ */
+public interface Adapter {}
