c3a3c21637ce798a12b4c9934c04bb2b4c582bc4
Nick Knudson <nickaknudson@gmail.com>
2014-03-13 16:17:38 -0700

    callbacks can be null

--- src/com/nickaknudson/mva/clients/async/AsyncCacheClient.java
+++ src/com/nickaknudson/mva/clients/async/AsyncCacheClient.java
@@ -1,7 +1,6 @@
 package com.nickaknudson.mva.clients.async;
 
 import java.io.File;
-import java.security.InvalidParameterException;
 
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.AsyncHttpResponse;
@@ -19,7 +18,6 @@ public abstract class AsyncCacheClient implements CacheClient {
 	 * @param callback
 	 */
 	public void cache(String url, String filename, final CacheCallback callback) {
-		if(callback == null) throw new InvalidParameterException("callback cannot be null");
 		AsyncHttpClient.getDefaultInstance().getFile(url, filename, new AsyncHttpClient.FileCallback() {
 			@Override
 			public void onCompleted(Exception e, AsyncHttpResponse source, File result) {
